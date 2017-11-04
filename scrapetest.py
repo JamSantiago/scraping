cfcbe77b3427a40d9bbf7e2b299a03675f3f69aa
Jam Santiago <jamsantiago@MacBook-Air-de-Jam.local>
2017-11-04 17:59:21 -0300

    Adicionando o BeautilfulSoup4

--- scrapetest.py
+++ scrapetest.py
@@ -1,5 +1,7 @@
 from urllib.request import urlopen
+from bs4 import BeautifulSoup
 
 html = urlopen("http://pythonscraping.com/pages/page1.html")
+bsObj = BeautifulSoup(html.read(), 'html.parse')
 
-print(html.read())
+print(bsObj.h1)
